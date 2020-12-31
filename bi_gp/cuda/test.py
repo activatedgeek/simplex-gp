a3f8e0369534bb12bfd31df83b6c6f54c75de7e3
Sanyam Kapoor <sanyam@nyu.edu>
2020-12-31 03:47:34 -0500

    fix blur bottlenecks, reduce number of cuda threads

--- bi_gp/cuda/test.py
+++ bi_gp/cuda/test.py
@@ -6,7 +6,7 @@ from timeit import default_timer as timer
 
 
 def test_cpu(root, src, ref, cdebug=False):
-  cpu_lattice = load(name="cpu_lattice",
+  cpu_lattice = load(name=f'cpu_lattice{"_debug" if cdebug else ""}',
                      verbose=cdebug,
                      extra_cflags=['-DDEBUG'] if cdebug else None,
                      sources=[(root / '..' / 'lattice.cpp')])
@@ -15,10 +15,8 @@ def test_cpu(root, src, ref, cdebug=False):
 
   res = cpu_lattice.filter(src, ref, 1)
 
-  print(f'CPU finished in: {(timer() - start):.6f}s')
-
-  return res
-
+  ts = timer() - start
+  return res, ts
 
 def test_gpu(root, src, ref, cdebug=False):
   device = 'cuda:0' if torch.cuda.is_available() else None
@@ -28,7 +26,7 @@ def test_gpu(root, src, ref, cdebug=False):
   src = src.to(device)
   ref = ref.to(device)
 
-  gpu_lattice = load(name="gpu_lattice",
+  gpu_lattice = load(name=f'gpu_lattice{"_debug" if cdebug else ""}',
                      verbose=cdebug,
                      extra_cflags=['-DDEBUG'] if cdebug else None,
                      extra_cuda_cflags=['-DDEBUG'] if cdebug else None,
@@ -41,10 +39,8 @@ def test_gpu(root, src, ref, cdebug=False):
 
   res = gpu_lattice.filter(src, ref, 1)
   
-  print(f'GPU finished in: {(timer() - start):.6f}s')
-
-  return res
-
+  ts = timer() - start
+  return res, ts
 
 def main(n=1000, d=10, cdebug=True):
   root = Path(os.path.dirname(__file__))
@@ -55,24 +51,39 @@ def main(n=1000, d=10, cdebug=True):
 
   print(f'N: {ref.size(0)}, pD: {ref.size(1)}')
 
-  res_cpu = test_cpu(root, src, ref, cdebug=cdebug)
+  res_cpu, ts_cpu = test_cpu(root, src, ref, cdebug=cdebug)
 
   print('-------------------------------')
 
-  res_gpu = test_gpu(root, src, ref, cdebug=cdebug).cpu()
+  res_gpu, ts_gpu = test_gpu(root, src, ref, cdebug=cdebug)
+  res_gpu = res_gpu.cpu()
 
   print('-------------------------------')
 
-  ## May still be small enough to be ok.
-  rel_err = (res_cpu - res_gpu).norm(p=2) / res_cpu.norm(p=2)
-  abs_rel_err = (res_cpu - res_gpu).norm(p=1) / res_cpu.norm(p=1)
-  print(f'Rel. Err.: {rel_err}')
-  print(f'Abs. Rel. Err.: {abs_rel_err}')
-  
-  # print('CPU:')
-  # print(res_cpu)
-  # print('GPU:')
-  # print(res_gpu)
+  try:
+    assert torch.allclose(res_cpu, res_gpu), 'CPU/GPU mismatch!'
+    print('Matched!')
+  except AssertionError as aexc:
+    rel_err = (res_cpu - res_gpu).norm(p=2) / res_cpu.norm(p=2)
+    abs_rel_err = (res_cpu - res_gpu).norm(p=1) / res_cpu.norm(p=1)
+
+    delta_mu = (res_cpu - res_gpu).abs().mean()
+    delta_std = (res_cpu - res_gpu).abs().std()
+
+    print(f'Rel. Err.: {rel_err}')
+    print(f'Abs. Rel. Err.: {abs_rel_err}')
+    print(f'Pointwise Abs. Err.: {delta_mu} +/- {delta_std}')
+    
+    print('CPU Output:')
+    print(res_cpu)
+    print('GPU Output:')
+    print(res_gpu)
+
+    ## Relative errors may still be small enough to be ok.
+    print(aexc)
+
+  print(f'Speedup: ~{ts_cpu / ts_gpu:.2f}x')
+
 
 if __name__ == "__main__":
   from fire import Fire
