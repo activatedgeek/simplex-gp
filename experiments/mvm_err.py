9d8c3a7cdcbe73bfe1c39981b3d6553c772c79f5
Sanyam Kapoor <sanyam@nyu.edu>
2021-01-26 13:48:27 -0500

    fix time logging on cuda

--- experiments/mvm_err.py
+++ experiments/mvm_err.py
@@ -1,7 +1,7 @@
 import os
 import torch
 import wandb
-from timeit import default_timer as timer
+# from timeit import default_timer as timer
 from gpytorch.kernels import RBFKernel, MaternKernel
 
 from bi_gp.bilateral_kernel import MaternLattice, RBFLattice
@@ -12,7 +12,7 @@ def rel_err(x,y):
     return ((x-y)**2).mean().sqrt()/((x**2).mean().sqrt()+(y**2).mean().sqrt())
 
 def main(dataset: str = None, data_dir: str = None, seed: int = None, device: int = 0,
-         kern: str = None, nu: float = None, order: int = 1, ell: float = 1.0):
+         kern: str = None, nu: float = None, order: int = 1, ell: float = 1.0, n: int = None):
     wandb.init(config={
       'kernel': kern,
       'dataset': dataset,
@@ -21,34 +21,63 @@ def main(dataset: str = None, data_dir: str = None, seed: int = None, device: in
       'lengthscale': ell,
     })
 
-    assert kern in ['rbf', 'matern'], "Invalid kernel"
+    assert kern in ['rbf', 'mat'], "Invalid kernel"
 
     set_seeds(seed)
     device = f"cuda:{device}" if (device >= 0 and torch.cuda.is_available()) else "cpu"
 
     data_iter = prepare_dataset(dataset, uci_data_dir=data_dir, device=device, train_val_split=1.0)
     _, X, y = next(data_iter)
+    if n is not None:
+      perm = torch.randperm(n)
+      X, y = X[perm], y[perm]
 
     if kern == "rbf":
       K_gt = RBFKernel().to(device)
       K_lattice = RBFLattice(order=order).to(device)
-    else:
+    elif kern == "mat":
       K_gt = MaternKernel(nu=nu).to(device)
       K_lattice = MaternLattice(nu=nu, order=order).to(device)
+    else:
+      raise NotImplementedError
 
     K_gt.lengthscale = K_lattice.lengthscale = ell
 
-    t_start = timer()
-    mvm_gt = K_gt(X, X) @ y
-    ref_ts = timer() - t_start
+    start = torch.cuda.Event(enable_timing=True)
+    end = torch.cuda.Event(enable_timing=True)
+    start.record()
+    # start = timer()
 
-    t_start = timer()
+    mvm_gt = K_gt(X, X) @ y
+
+    # end = timer()
+    end.record()
+    torch.cuda.current_stream().synchronize()
+
+    ## To build cache
     mvm_lattice = K_lattice(X, X) @ y
-    lattice_ts = timer() - t_start
+    torch.cuda.current_stream().synchronize()
+
+    start2 = torch.cuda.Event(enable_timing=True)
+    end2 = torch.cuda.Event(enable_timing=True)
+    start2.record()
+    # start2 = timer()
+
+    mvm_lattice = K_lattice(X, X) @ y
+
+    # end2 = timer()
+    end2.record()
+    torch.cuda.current_stream().synchronize()
+
+    # wandb.log({ 'ts/ref': end - start })
+    # wandb.log({ 'ts/lattice': end2 - start2 })
+
+    wandb.log({ 'ts/ref': start.elapsed_time(end) / 1000 })
+    wandb.log({ 'ts/lattice': start2.elapsed_time(end2) / 1000 })
 
     err = rel_err(mvm_gt,mvm_lattice/(mvm_lattice/mvm_gt).mean())
 
-    wandb.log({ 'err/rel_err': err, 'ts/ref': ref_ts, 'ts/lattice': lattice_ts })
+    wandb.log({ 'err/rel_err': err })
 
 
 if __name__ == "__main__":
